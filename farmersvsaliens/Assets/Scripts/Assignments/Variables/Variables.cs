e54fba543e0caf3224f854ee2304758c59a705f1
beccastolethefrosting <10885024@my.uvu.edu>
2020-01-15 17:17:17 -0700

    Commit: The final changes

--- farmersvsaliens/Assets/Scripts/Assignments/Variables/Variables.cs
+++ farmersvsaliens/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
+    //Delaration
+    public int cookieBox;
+    public float waterBottle;
+    private string firstName;
+    public double rbi;
+    public GameObject player;
+
+    public float amount;
+    public float money;
+    public float cost;
+
+
+    //   Incoming ROFLCOPTER!
+
+    //     ROFL:ROFL:ROFL:ROFL
+    //         _____^_____
+    //    L __/        [] \    
+    //   LOL===__          \ 
+    //    L      \__________]
+    //               I I
+    //           ----------/
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cookies(amount,money,cost);
+
+
+
+        cookieBox = 20;
+        firstName = "Blob";
+        Debug.Log("How many cookies did I steal from the jar " + cookieBox);
+        //Function Call
+        CheckScore();
     }
 
     // Update is called once per frame
@@ -15,4 +47,22 @@ public class Variables : MonoBehaviour
     {
         
     }
+
+    void Cookies(float amount, float money, float cost)
+    {
+        print("yo they sellin' biscuits");
+
+        float total;
+        total = money - cost * amount;
+        print(total);
+    }
+
+    void CheckScore()
+    {
+        print("Hello there.");
+
+    }
+
+
+
 }
