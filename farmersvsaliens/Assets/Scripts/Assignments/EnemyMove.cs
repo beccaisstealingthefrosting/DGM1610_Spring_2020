84add3cb23468fe593df7bd80baddea492c8ad43
beccastolethefrosting <10885024@my.uvu.edu>
2020-02-24 17:10:33 -0700

    edited enemy move, need to fix spawnmanager

--- farmersvsaliens/Assets/Scripts/Assignments/EnemyMove.cs
+++ farmersvsaliens/Assets/Scripts/Assignments/EnemyMove.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     public GameObject player;
-    public Transform target;
-    public int damage;
+    //public Transform target;
+    private Rigidbody enemyRb;
+    //public int damage;
     public float moveSpeed;
     
 
@@ -15,14 +16,31 @@ public class EnemyMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
-        
+        //target = GameObject.Find("Player").transform;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
+        //transform.LookAt(target);
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
     }
+
+    void FixedUpdate()
+    {
+        enemyRb.AddForce((PlayerPrefs.transform.position - transform.position).normalized * moveSpeed);
+
+
+    }
+
+
+
+
+
+
+
+
 }
--- farmersvsaliens/Assets/Scripts/Assignments/SpawnManager.cs
+++ farmersvsaliens/Assets/Scripts/Assignments/SpawnManager.cs
@@ -26,4 +26,5 @@ public class SpawnManager : MonoBehaviour
         }
 
     }
-}
+
+ }
