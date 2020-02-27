5da1dbed815456f8d611148538c906972779d0f7
beccastolethefrosting <10885024@my.uvu.edu>
2020-02-26 17:18:51 -0700

    Enemy health and bullets

---
+++ farmersvsaliens/Assets/Scripts/Assignments/EnemyHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int currentHealth;
+    public const maxHealth = 3;
+    public Transform spawnPoint;
+    public int points;
+
+    
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+       public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            print("enemy is dead");
+            //scoreManager.AddPoints(points);
+            transform.position = spawnPoint.position;
+            transform.rotation = spawnPoint.rotation;
+            currentHealth = maxHealth;
+        }
+    }
+
+}
